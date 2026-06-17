fd7dff0f3c5faff92355dba3a925f0905b5e2f9e
Mirko Bozzetto <mirko@mirko.re>
2026-06-17 15:14:41 +0200

    fix(ios): drop unnecessary unsafe block in document picker

--- src/platform/ios/picker.rs
+++ src/platform/ios/picker.rs
@@ -102,7 +102,7 @@ async fn present_picker(
     let delegate = PickerDelegate::new(mtm);
     picker.setDelegate(Some(ProtocolObject::from_ref(delegate.deref())));
     picker.setAllowsMultipleSelection(false);
-    unsafe {
+    {
         let docs = super::documents_dir();
         let dir_url = NSURL::fileURLWithPath(&NSString::from_str(
             &docs.to_string_lossy(),
